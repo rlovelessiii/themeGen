8bd26f204750e9efa540b8f2c07307b1cc61817a
Richard Loveless III <richardlovelessiii@gmail.com>
2019-11-11 19:18:25 -0500

    clean-up

--- launch.sh
+++ launch.sh
@@ -10,11 +10,12 @@ OS=$(uname)
 
 # Execute script correspoding to operating system
 # DISCLAIMER: Only tested on Manjaro-i3 and MacOS 10.14 - 10.15
-if [[ ${OS} == "Linux" ]]; then
+if [[ ${OS} == "Linux" ]]
+ then
     if zenity --question \
-	--title="Theme Generator" \
-	--text="Update theme?" \
-	--no-wrap;
+      --title="Theme Generator" \
+      --text="Update theme?" \
+      --no-wrap
     then
       . "${DIR}/manjaro_i3.sh"
       update_wallpapers && \
@@ -23,10 +24,10 @@ if [[ ${OS} == "Linux" ]]; then
       update_remote && \
       restart_logout
     fi
-elif [[ ${OS} == "Darwin" ]];
+elif [[ ${OS} == "Darwin" ]]
 then
     . "${DIR}/macos.sh"
-    update_wallpapers && apply_theme ${1}
+    update_wallpapers && apply_theme "${1}"
 else
-    echo "OOPS! OS is not recognized. Please submit a bug report to RLovelessIII :)";
+    echo "OOPS! OS is not recognized. Please submit a bug report to RLovelessIII :)"
 fi
